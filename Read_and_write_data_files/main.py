c5bcb5cbc1cdb88b791f401c07636f7194948af4
dhanushngp <24243019@drngpit.ac.in>
2026-03-04 12:49:15 +0530

    none

--- Read_and_write_data_files/main.py
+++ Read_and_write_data_files/main.py
@@ -3,5 +3,5 @@ import pandas as pd
 file = pd.read_csv(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data.csv")
 print(file)
 
-file.to_csv(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data2.csv")
+file.to_csv(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data2.csv")#,Index = False 
 print(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data2.csv")
