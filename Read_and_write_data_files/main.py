e2a9c8ae94cf9ec9249cdbc8078f05d7dddfe1e1
dhanushngp <24243019@drngpit.ac.in>
2026-03-04 12:47:29 +0530

    exp1

---
+++ Read_and_write_data_files/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+file = pd.read_csv(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data.csv")
+print(file)
+
+file.to_csv(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data2.csv")
+print(r"C:\Users\student\DSA-lab\DSA-lab\Read_and_write_data_files\data2.csv")
